cbf01ed686ef1c857515c1ccd83af31fbffa69f9
Abhi <abhishek953dash@gmail.com>
2022-01-06 21:20:57 +0530

    Accordion complete

---
+++ my-app/src/App.js
@@ -0,0 +1,32 @@
+import './App.css';
+import {useState} from 'react';
+import data from './data/placesData.json';
+
+function App() {
+  const [select,setSelect]=useState(null)
+  const toggle=(i)=>{
+    if(select===i){
+      return setSelect(null)
+    }
+    setSelect(i)
+  }
+  return (
+    <>
+    <div className='container'>
+      <div className="accordion">
+          {data.map((ele,i) => (
+            <div className='item' onClick={()=>toggle(i)}>
+              <div className='title'> <h2> {ele.place} </h2> <span>{select===i ? '-':'+'}</span> </div>
+              <div className={select===i ? 'desc show':'desc'}> {ele.about}</div>
+            </div>
+
+          ))}
+        
+
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default App;
